ebdbd0094bb6c8f7afc60c4a3094284339b42480
Audrey McGowan <audrey@periscopedata.com>
2017-10-10 09:14:43 -0700

    adding null constraint should be safety assured

--- lib/strong_migrations/migration.rb
+++ lib/strong_migrations/migration.rb
@@ -51,6 +51,9 @@ module StrongMigrations
           if postgresql? && index_value
             raise_error :add_reference
           end
+        when :change_column_null
+          unsafe = args[3].nil? && !args[2]
+          raise_error :change_column_null_without_default if unsafe
         when :execute
           raise_error :execute
         end
@@ -165,6 +168,9 @@ you're doing is safe before proceeding, then wrap it in a safety_assured { ... }
 "The force option will destroy existing tables.
 If this is intended, drop the existing table first.
 Otherwise, remove the option."
+        when :change_column_null_without_default
+"When setting a null constraint on a column, a fourth argument to set
+existing nulls with some other value is recommended otherwise null rows will be invalid"
         when :execute
 "The strong_migrations gem does not support inspecting what happens inside an
 execute call, so cannot help you here. Please make really sure that what
