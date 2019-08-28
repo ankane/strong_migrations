09654e98d86cd29e4efc2eeb51a9ec043d26c98a
Andrew Kane <andrew@chartkick.com>
2019-08-28 11:47:03 -0700

    More robust code - #88

--- lib/strong_migrations/migration.rb
+++ lib/strong_migrations/migration.rb
@@ -1,29 +1,30 @@
 module StrongMigrations
   module Migration
-    def initialize(*args)
-      super
-      @checker = StrongMigrations::Checker.new(self)
-    end
-
     def migrate(direction)
-      @checker.direction = direction
+      strong_migrations_checker.direction = direction
       super
     end
 
     def method_missing(method, *args)
-      @checker.perform(method, *args) do
+      strong_migrations_checker.perform(method, *args) do
         super
       end
     end
 
     def safety_assured
-      @checker.safety_assured do
+      strong_migrations_checker.safety_assured do
         yield
       end
     end
 
     def stop!(message, header: "Custom check")
       raise StrongMigrations::UnsafeMigration, "\n=== #{header} #strong_migrations ===\n\n#{message}\n"
+    end
+
+    private
+
+    def strong_migrations_checker
+      @strong_migrations_checker ||= StrongMigrations::Checker.new(self)
     end
   end
 end
