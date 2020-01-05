9467ac821b9f1809cddbf568d8942d7faa5bf46c
Andrew Kane <andrew@chartkick.com>
2020-01-05 04:15:38 -0800

    Fixed flaky test

--- test/strong_migrations_test.rb
+++ test/strong_migrations_test.rb
@@ -22,6 +22,10 @@ class AddIndexUp < TestMigration
   def self.up
     add_index :users, :name
   end
+
+  def self.down
+    remove_index :users, :name
+  end
 end
 
 class AddIndexSafePostgres < TestMigration
