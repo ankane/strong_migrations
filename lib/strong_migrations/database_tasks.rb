345e8ddbf9ea5e382179d2a3ac2ea8c08041655a
Andrew Kane <andrew@chartkick.com>
2020-11-20 19:06:21 -0800

    Added friendly message for lock timeouts

--- lib/strong_migrations/database_tasks.rb
+++ lib/strong_migrations/database_tasks.rb
@@ -3,14 +3,31 @@ module StrongMigrations
     def migrate
       super
     rescue => e
-      if e.cause.is_a?(StrongMigrations::Error)
+      lock_messages = ["canceling statement due to lock timeout", "Lock wait timeout exceeded"]
+      lock_timeout = e.cause && lock_messages.any? { |m| e.cause.message.include?(m) }
+
+      if e.cause.is_a?(StrongMigrations::Error) || lock_timeout
         # strip cause and clean backtrace
         def e.cause
           nil
         end
 
-        def e.message
-          super.sub("\n\n\n", "\n\n") + "\n"
+        if lock_timeout
+          def e.message
+            <<~EOS
+              #{super.split("\n").first}
+
+              === Lock timeout detected #strong_migrations ===
+
+              This typically happens when a table is busy.
+              Try again at a lower traffic time.
+
+            EOS
+          end
+        else
+          def e.message
+            super.sub("\n\n\n", "\n\n") + "\n"
+          end
         end
 
         unless Rake.application.options.trace
