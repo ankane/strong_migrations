fc2568249b079ab770dccfcc0cbca9718a04394c
Charles Delannoy <charles.delannoy@doctolib.com>
2021-11-08 11:22:10 +0100

    Using *agrs instead of version to make this retrocompatible + also undo Gemfile changes for ActiveRecord

--- lib/strong_migrations/database_tasks.rb
+++ lib/strong_migrations/database_tasks.rb
@@ -1,7 +1,7 @@
 module StrongMigrations
   module DatabaseTasks
-    def migrate(version = nil)
-      super(version)
+    def migrate(*args)
+      super(args)
     rescue => e
       if e.cause.is_a?(StrongMigrations::Error)
         # strip cause and clean backtrace
