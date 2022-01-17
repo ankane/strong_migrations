87ca856efe169bab32930e607d9168f53626a9dd
Andrew Kane <andrew@ankane.org>
2022-01-16 23:52:08 -0500

    DRY tests [skip ci]

--- test/add_foreign_key_test.rb
+++ test/add_foreign_key_test.rb
@@ -10,13 +10,13 @@ class AddForeignKeyTest < Minitest::Test
   end
 
   def test_add_foreign_key_safe
-    skip "Active Record 6.0.3 bug" if (mysql? || mariadb?) && ActiveRecord::VERSION::STRING.start_with?("6.0.3")
+    skip "Active Record 6.0.3 bug" if ar_bug? && (mysql? || mariadb?)
 
     assert_safe AddForeignKeySafe
   end
 
   def test_add_foreign_key_validate_same_transaction
-    skip "Active Record 6.0.3 bug" if ActiveRecord::VERSION::STRING.start_with?("6.0.3")
+    skip "Active Record 6.0.3 bug" if ar_bug?
 
     skip unless postgresql?
 
@@ -24,10 +24,14 @@ class AddForeignKeyTest < Minitest::Test
   end
 
   def test_add_foreign_key_validate_no_transaction
-    skip "Active Record 6.0.3 bug" if ActiveRecord::VERSION::STRING.start_with?("6.0.3")
+    skip "Active Record 6.0.3 bug" if ar_bug?
 
     skip unless postgresql?
 
     assert_safe AddForeignKeyValidateNoTransaction
+  end
+
+  def ar_bug?
+    ActiveRecord::VERSION::STRING.start_with?("6.0.3")
   end
 end
