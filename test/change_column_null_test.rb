58ce2fba656a8939f3df4989879cde267c1772dc
Ryota Arai <ryota.arai@gmail.com>
2022-02-16 23:30:29 +0900

    Allow setting NOT NULL on MySQL with online DDL.

--- test/change_column_null_test.rb
+++ test/change_column_null_test.rb
@@ -2,10 +2,24 @@ require_relative "test_helper"
 
 class ChangeColumnNullTest < Minitest::Test
   def test_basic
-    if postgresql? || mysql? || mariadb?
+    if postgresql?
       assert_unsafe ChangeColumnNull
-    else
-      assert_safe ChangeColumnNull
+    elsif mysql?
+      with_target_version("5.6.0") do
+        assert_safe ChangeColumnNull
+      end
+    elsif mariadb?
+      with_target_version("10.0.0") do
+        assert_safe ChangeColumnNull
+      end
+    end
+  end
+
+  def test_old_mysql
+    skip unless mysql? || mariadb?
+
+    with_target_version("5.5.0") do
+      assert_unsafe ChangeColumnNull
     end
   end
 
@@ -34,7 +48,17 @@ class ChangeColumnNullTest < Minitest::Test
   end
 
   def test_default
-    assert_unsafe ChangeColumnNullDefault
+    if postgresql?
+      assert_unsafe ChangeColumnNullDefault
+    elsif mysql?
+      with_target_version("5.6.0") do
+        assert_unsafe ChangeColumnNullDefault
+      end
+    elsif mariadb?
+      with_target_version("10.0.0") do
+        assert_unsafe ChangeColumnNullDefault
+      end
+    end
   end
 
   def test_constraint_methods
@@ -51,5 +75,21 @@ class ChangeColumnNullTest < Minitest::Test
     with_target_version(12) do
       assert_safe ChangeColumnNullQuoted
     end
+  end
+
+  def test_mysql_non_strict_mode
+    skip unless mysql? || mariadb?
+
+    version = if mysql?
+      "5.6.0"
+    elsif mariadb?
+      "10.0.0"
+    end
+
+    StrongMigrations.target_mysql_sql_modes = []
+    with_target_version(version) do
+      assert_unsafe ChangeColumnNull
+    end
+    StrongMigrations.target_mysql_sql_modes = nil
   end
 end
