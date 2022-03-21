f4873d1c1951d551ea72ff8ecacd3711fa6bd03f
Andrew Kane <andrew@ankane.org>
2022-03-21 15:55:06 -0700

    Improved code

--- lib/strong_migrations/migrator.rb
+++ lib/strong_migrations/migrator.rb
@@ -1,20 +1,18 @@
 module StrongMigrations
   module Migrator
     def ddl_transaction(migration, *args)
-      if use_transaction?(migration) && StrongMigrations.lock_timeout_retries > 0
-        # handle MigrationProxy class
-        migration = migration.send(:migration) if migration.respond_to?(:migration, true)
+      return super unless StrongMigrations.lock_timeout_retries > 0 && use_transaction?(migration)
 
-        # retry migration since the entire transaction needs to be rerun
-        checker = migration.send(:strong_migrations_checker)
-        checker.retry_lock_timeouts(check_committed: true) do
-          # failed transaction reverts timeout, so need to re-apply
-          checker.timeouts_set = false
+      # handle MigrationProxy class
+      migration = migration.send(:migration) if migration.respond_to?(:migration, true)
 
-          super(migration, *args)
-        end
-      else
-        super
+      # retry migration since the entire transaction needs to be rerun
+      checker = migration.send(:strong_migrations_checker)
+      checker.retry_lock_timeouts(check_committed: true) do
+        # failed transaction reverts timeout, so need to re-apply
+        checker.timeouts_set = false
+
+        super(migration, *args)
       end
     end
   end
