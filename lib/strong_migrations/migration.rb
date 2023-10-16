ef01e2c352e27b98c7debca8ab7f1ea6e2fa72b7
Josh Menden <josh.menden@hey.com>
2023-10-15 22:22:08 -0600

    Override revert method to check for version safety

--- lib/strong_migrations/migration.rb
+++ lib/strong_migrations/migration.rb
@@ -20,6 +20,14 @@ module StrongMigrations
     end
     ruby2_keywords(:method_missing) if respond_to?(:ruby2_keywords, true)
 
+    def revert(*migration_classes, &block)
+      if strong_migrations_checker.version_safe?
+        safety_assured { super(*migration_classes, &block) }
+      else
+        super(*migration_classes, &block)
+      end
+    end
+
     def safety_assured
       strong_migrations_checker.class.safety_assured do
         yield
