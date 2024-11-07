3f1730164253f39c737371529268ad25e5cfb669
Andrew Kane <andrew@ankane.org>
2024-11-07 04:58:33 -0800

    Added comment [skip ci]

--- lib/strong_migrations/safe_methods.rb
+++ lib/strong_migrations/safe_methods.rb
@@ -47,6 +47,8 @@ module StrongMigrations
     def safe_add_foreign_key(from_table, to_table, *args, **options)
       @migration.reversible do |dir|
         dir.up do
+          # validate option is ignored for Active Record < 7.1
+          # https://github.com/rails/rails/pull/45896
           if ar_version < 7.1 || !connection.foreign_key_exists?(from_table, to_table, **options.merge(validate: false))
             @migration.add_foreign_key(from_table, to_table, *args, **options.merge(validate: false))
           end
